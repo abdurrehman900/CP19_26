3e53d7debcf8603b67efa7ca60195be643b72858
abdurrehman900 <49781985+abdurrehman900@users.noreply.github.com>
2019-04-21 20:30:54 +0500

    Add files via upload
    Fabonacci number

---
+++ Fabonacci number.py
@@ -0,0 +1,17 @@
+#problem4
+
+def fab():
+	b=0
+	print(b,end=",")
+	c=1
+	print(c,end=",")
+	d=b+c
+	for i in range(1,a):
+		print(d,end=",")
+		b=c
+		c=d
+		d=b+c
+				
+		
+a=int(input("number"))
+fab()
