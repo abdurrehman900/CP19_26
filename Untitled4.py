f9ca97fa33160b7f8f83dbc44be325a3eb57197d
nishatahmed12 <49781748+nishatahmed12@users.noreply.github.com>
2019-04-21 22:24:25 +0500

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[1]:
+
+Number = int(input("Please Enter any Number: "))    
+Reverse = 0    
+while(Number > 0):    
+    Reminder = Number %10    
+    Reverse = (Reverse *10) + Reminder    
+    Number = Number //10    
+     
+print("\n Reverse of entered number is = %d" %Reverse)   
+
